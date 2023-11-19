d342e7fa398ce37a8bd6e4997a75c770418d8927
5p1k3-258 <s5416359@bournemouth.ac.uk>
2023-11-19 03:19:34 +0000

    Added snap points
    prefabs (that need a bit of editing tbh) that will let draggable blocks to snap to them (currently an unlimited amount of draggable blocks can be linked to a snap point, will fix that soon)

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -6,14 +6,25 @@ namespace BFX
 {
     public class Drag : MonoBehaviour
     {
+        // Variables for dragging
         private bool dragging = false;
         private Vector3 offset;
+        // Variables for snapping to snap points
+        public SnapPoint snapPoint;
+        public bool snapped = false;
 
         void Update()
         {
-        if (dragging)
+            if (dragging)
             {
                 transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+            }
+            if (!dragging && snapped)
+            {
+                if (snapPoint != null)
+                {
+                    transform.position = snapPoint.transform.position;
+                }
             }
         }
 
@@ -21,11 +32,42 @@ namespace BFX
         {
             offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
             dragging = true;
+            if (snapped)
+            {
+                snapPoint.taken = false;
+                snapPoint.snappedObject = null;
+            }
         }
 
         private void OnMouseUp()
         {
             dragging = false;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Debug.Log("2DCEnter");
+        }
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            Debug.Log("2DCStay");
+        }
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            Debug.Log("2DCExit");
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log("CEnter");
+        }
+        private void OnCollisionStay(Collision collision)
+        {
+            Debug.Log("CStay");
+        }
+        private void OnCollisionExit(Collision collision)
+        {
+            Debug.Log("CExit");
         }
     }
 }
---
+++ Assets/SnapPoint.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BFX
+{
+    public class SnapPoint : MonoBehaviour
+    {
+        public GameObject snappedObject;
+        public bool taken;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            // check if collided objcet is a draggable by looking for draggable script
+            if (other.gameObject.GetComponent<Drag>())
+            {
+                Drag client = other.gameObject.GetComponent<Drag>();
+                client.snapped = true;
+                client.snapPoint = this;
+                taken = true;
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.GetComponent<Drag>())
+            {
+                Drag client = other.gameObject.GetComponent<Drag>();
+                client.snapped = false;
+                client.snapPoint = null;
+                taken = false;
+            }
+        }
+    }
+}
